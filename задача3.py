fca82687b4a8b484a95f4ac09f228d3d3e45410f
Шишков Александр <134552506+alex133060@users.noreply.github.com>
2024-03-30 11:28:55 +0300

    Add files via upload

---
+++ задача3.py
@@ -0,0 +1,17 @@
+a=open('rocket.csv',encoding='utf-8').readlines()
+a.pop(0)
+for i in range(len(a)):
+    a[i]=a[i].strip().split(',')
+    print(*a[i])
+while True:
+    date=input()
+    if date=='sleep':
+        break
+    flag=0
+    for x in a:
+        if x[0]==date:
+            print(f'Шифр {x[1]} от {x[2]} был получен {x[0]}')
+            flag=1
+            break
+    if flag==0:
+        print('Ничего не найдено')

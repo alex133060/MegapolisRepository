4ff04561736cdf48dc37b38fc4ca128e64d704e1
Шишков Александр <134552506+alex133060@users.noreply.github.com>
2024-03-30 11:47:55 +0300

    Add files via upload

---
+++ задача5.py
@@ -0,0 +1,26 @@
+def hash(s):
+    """
+    данная функция создаёт хеш для данных о частях ракеты
+    """
+    s=s[1]
+    p=67
+    m=10**9+9
+    h=0
+    for i in range(len(s)):
+        h+=A.find(s[i])*p**i
+    h=h%m
+    return str(h)
+
+A='АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+a=A.lower()
+A='*'+A+a+' '
+a=open('rocket.csv.csv',encoding='utf-8').readlines()
+shapka=a.pop(0)
+for i in range(len(a)):
+    a[i]=a[i].strip().split()
+
+f=open('students_with_hash.csv','w',encoding='utf-8')
+f.write(shapka)
+for x in a:
+    x[0]=hash(x)
+    f.write(','.join(x)+'\n')
